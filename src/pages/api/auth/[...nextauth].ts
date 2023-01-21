a8d0f09752df85ba50edf8536e62ba8b2256d96a
max <max.stachalski@andrena.de>
2023-01-21 15:55:56 +0100

    Use Prisma to confirm User Credentials

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,38 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "../../../../lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export default NextAuth({
   secret: "ABC",
+  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+        if (user && user.password === credentials?.password) {
           // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
+          if (!user && credentials) {
+            return await prisma.user.create({
+              data: {
+                email: credentials.email,
+                password: credentials.password,
+              },
+            });
+          }
           // If you return null then an error will be displayed advising the user to check their details.
           return null;
 
