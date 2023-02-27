2f265ce32983813f1dbf2592a604543561be8a5b
max <max.stachalski@andrena.de>
2023-02-27 12:39:50 +0100

    Aufgabe init

--- src/pages/pitch/index.tsx
+++ src/pages/pitch/index.tsx
@@ -9,57 +9,26 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Pitch() {
-  const topicSchema = z.object({
-    author: z.object({
-      firstName: z
-        .string({ invalid_type_error: "Input must be a string" })
-        .min(1, { message: "Must enter at least one character" })
-        .refine((value) => value.match(/^[a-zA-Z]+$/), {
-          message: "Numbers are not allowed",
-        }),
-      lastName: z.string().min(1),
-    }),
-    topic: z.object({
-      title: z.string().min(1),
-      description: z.string().max(150).optional(),
-      type: z.enum([
-        "Hands-On",
-        "Vortrag",
-        "Lightning Talk",
-        "Diskussionsrunde",
-      ]),
-    }),
-  });
+  //Todo
+  const topicSchema = z.object();
 
   type IFormData = z.infer<typeof topicSchema>;
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm<IFormData>({
+  //TODO Felder aus useForm holen und hier destructuren
+  const {  } = useForm<IFormData>({
     resolver: zodResolver(topicSchema),
   });
-
-  const submitForm: SubmitHandler<IFormData> = (data: IFormData) => {
-    console.log(JSON.stringify(data));
-  };
-
-  const onError: SubmitErrorHandler<IFormData> = (errors: FieldErrors) => {
-    console.log(errors);
-  };
 
   return (
     <main className={styles.main}>
       <form
         className={styles.form}
-        onSubmit={handleSubmit(submitForm, onError)}
+        onSubmit={() => null} //Todo
       >
         <div className={styles.inputContainer}>
-          {errors?.author?.firstName && (
-            <span>{errors.author.firstName.message}</span>
-          )}
+          {
+            //TODO fehlerLabel
+          }
           <input
             type="text"
             id={styles.fName}
@@ -68,9 +37,9 @@ export default function Pitch() {
           />
         </div>
         <div className={styles.inputContainer}>
-          {errors?.author?.lastName && (
-            <span>{errors.author.lastName.message}</span>
-          )}
+          {
+            //TODO Fehlerlabel
+          }
           <input
             type="text"
             id={styles.lName}
@@ -79,7 +48,9 @@ export default function Pitch() {
           />
         </div>
         <div className={styles.inputContainer}>
-          {errors?.topic?.title && <span>{errors.topic.title.message}</span>}
+          {
+            //Todo fehlerlabel
+          }
           <input
             type="text"
             id={styles.topicTitle}
@@ -88,9 +59,9 @@ export default function Pitch() {
           />
         </div>
         <div className={styles.inputContainer}>
-          {errors?.topic?.description && (
-            <span>{errors.topic.description.message}</span>
-          )}
+          {
+            //TODO Fehlerlabel
+          }
           <textarea
             wrap={"soft"}
             maxLength={150}
@@ -99,7 +70,9 @@ export default function Pitch() {
             {...register("topic.description")}
           />
           <span className={styles.charactersLeft}>
-            {150 - (watch("topic.description")?.length ?? 0)}
+            {
+              //TODO Anzeigen wieviele Zeichen noch für Description übrig sind
+            }
           </span>
         </div>
         <select
